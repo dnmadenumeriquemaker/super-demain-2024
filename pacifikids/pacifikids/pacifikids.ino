22ba8557880186f151b57160f246ec899deb7291
Kévin VENNITTI <kevin.vennitti@gmail.com>
2024-11-22 14:10:15 +0100

    Arduino code update

--- pacifikids/pacifikids/pacifikids.ino
+++ pacifikids/pacifikids/pacifikids.ino
@@ -1,4 +1,5 @@
-const int buttonPins[9] = {2, 3, 4, 5, 6, 7, 8, 9, 10};
+const int buttonPins[9] = {13, 7, 27, 35, 43, 51, 34, 42, 26};
+const int buttonLights[9] = {8, 2, 22, 30, 38, 46, 39, 47, 31};
 const int touchPins[2] = {11, 12}; // Modifier pour utiliser `touchRead` sur une carte compatible
 
 int buttonStates[9];
@@ -11,6 +12,7 @@ void setup() {
   // Configurer les pins des boutons en entrée
   for (int i = 0; i < 9; i++) {
     pinMode(buttonPins[i], INPUT_PULLUP); // INPUT_PULLUP pour éviter les résistances externes
+    pinMode(buttonLights[i], OUTPUT);
   }
 
   // Configurer les pins des bandes de cuivre (en entrée si non capacitif)
@@ -20,9 +22,20 @@ void setup() {
 }
 
 void loop() {
+  
+  //digitalWrite(buttonLights[0], HIGH);
+      
   // Lire l'état des boutons
   for (int i = 0; i < 9; i++) {
     buttonStates[i] = !digitalRead(buttonPins[i]); // Inverser car INPUT_PULLUP
+
+    digitalWrite(buttonLights[i], HIGH);
+
+    if (buttonStates[i] == HIGH) {
+      //digitalWrite(buttonLights[i], HIGH);
+    } else {
+      //digitalWrite(buttonLights[i], LOW);
+    }
   }
 
   // Lire l'état des bandes de cuivre
